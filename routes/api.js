253f071ddc4713080f2f8512f6cf1027d0605efe
ArturoRey89 <arturoreyes89@gmail.com>
2023-01-02 14:35:42 +0100

    feat: add database and schema

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,21 @@
 'use strict';
+const mongoose = require("mongoose");
+
+//Setup and connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log("Database connection error: ", error));
+db.on("connected", () => console.log("Connected to database"));
+
+const stockLikesSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  count: { type: Number, default: 1 },
+  ipAddress: [String],
+});
 
 module.exports = function (app) {
 
