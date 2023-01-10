e45ee357d6f627831355def8f250791f591000be
ArturoRey89 <arturoreyes89@gmail.com>
2023-01-10 01:47:20 +0100

    chor: 'prettier' formating

--- routes/api.js
+++ routes/api.js
@@ -1,119 +1,116 @@
-'use strict';
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
+"use strict"
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+const bcrypt = require("bcrypt")
+const mongoose = require("mongoose")
 
 //Setup and connect to database
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
 
-const db = mongoose.connection;
-db.on("error", (error) => console.log("Database connection error: ", error));
-db.on("connected", () => console.log("Connected to database"));
+const db = mongoose.connection
+db.on("error", (error) => console.log("Database connection error: ", error))
+db.on("connected", () => console.log("Connected to database"))
 
 const stockLikesSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: true, 
-    unique: true 
+  name: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  count: { 
-    type: Number, 
-    default: 1 
+  count: {
+    type: Number,
+    default: 1,
   },
   ipAddress: [String],
-}); 
+})
 
-const StockLikes = mongoose.model("StockLikes", stockLikesSchema);
-const ObjectId = mongoose.Types.ObjectId;
+const StockLikes = mongoose.model("StockLikes", stockLikesSchema)
+const ObjectId = mongoose.Types.ObjectId
 
 module.exports = function (app) {
+  app.route("/api/stock-prices").get(function (req, res) {
+    let { stock, like } = req.query
+    if (typeof stock === "undefined") {
+      return
+    }
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      let {stock, like} = req.query;
-      if(typeof stock === 'undefined') {
-        return 
-      }
+    if (Array.isArray(stock)) {
+      getStockPrice(stock[0])
+      getStockPrice(stock[1])
+      getLikes(stock[0])
+      getLikes(stock[1])
+    }
 
-      if( Array.isArray(stock) ) {
-        getStockPrice(stock[0]);
-        getStockPrice(stock[1]);
-        getLikes(stock[0]);
-        getLikes(stock[1]);
-      }
-
-      if (!Array.isArray(stock)) {
-        getStockPrice(stock);
-        getLikes(stock);
-        addLikes(stock, );
-      }
-    });
-
-};
+    if (!Array.isArray(stock)) {
+      Promise.all()
+      getStockPrice(stock)
+      getLikes(stock)
+      addLikes(stock, req.header("x-forwarded-for"))
+    }
+  })
+}
 
 const getStockPrice = (stockSymbol) => {
   const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`;
+    const xhr = new XMLHttpRequest()
+    const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockSymbol}/quote`
 
-    xhr.open("GET", url);
-    xhr.responseType = "json";
+    xhr.open("GET", url)
+    xhr.responseType = "json"
     xhr.onload = () => {
-      let { symbol, latestPrice } = JSON.parse(xhr.responseText);
-      resolve({ symbol: symbol, price: latestPrice });
-    };
+      let { symbol, latestPrice } = JSON.parse(xhr.responseText)
+      resolve({ symbol: symbol, price: latestPrice })
+    }
     xhr.onerror = () => {
-      reject("Stock value not returned");
-    };
-    xhr.send();
-  });
-  return promise;
-};
+      reject("Stock value not returned")
+    }
+    xhr.send()
+  })
+  return promise
+}
 
 const bcryptIP = (ipAddress) => {
   const promise = new Promise((resolve, reject) => {
     bcrypt.hash(ipAddress, process.env.SALT * 1, (err, hash) => {
       if (err) {
-        reject(err);
+        reject(err)
       }
       if (hash) {
-        resolve(hash);
+        resolve(hash)
       }
-    });
+    })
   })
-  return promise;
+  return promise
 }
 
 const getLikes = (stockName) => {
   const promise = new Promise((resolve, reject) => {
     StockLikes.find({ name: stockName }, (err, query) => {
       if (err) {
-        reject(err);
+        reject(err)
         console.log(err)
       }
       if (query.length == 0) {
-        resolve(0);
-        console.log(0);
+        resolve(0)
+        console.log(0)
       }
       if (query.length < 0) {
-        resolve(query[0].count);
-        console.log(query[0].count);
+        resolve(query[0].count)
+        console.log(query[0].count)
       }
-    });
+    })
   })
-  return promise;
+  return promise
 }
 
 const addLikes = (stockName, ipAddress) => {
   const promise = new Promise((resolve, reject) => {
     StockLikes.find({ name: stockName }, (err, query) => {
-
       if (err) {
-        reject(err);
-        console.log(err);
+        reject(err)
+        console.log(err)
       }
       //handle unregisterd stock
       if (query.length == 0) {
@@ -125,37 +122,36 @@ const addLikes = (stockName, ipAddress) => {
               ipAddress: [ipHash],
             },
             (err, stockLikes) => {
-              if (err){
+              if (err) {
                 reject(err)
-              }
-              else {
-                resolve(true);
+              } else {
+                resolve(true)
               }
             }
-          );
-        });
+          )
+        })
       }
-      // check if IP is new, and add like if new 
+      // check if IP is new, and add like if new
       if (query.length < 0) {
-        let ipIsUnique = true;
+        let ipIsUnique = true
         query[0].ipAddress.forEach((ipHash) => {
           bcrypt.compare(ipHash, ipAddress, (err, match) => {
-            if(err) {
+            if (err) {
               reject(err)
             }
             ipIsUnique = !match
-          });
-        });
+          })
+        })
         if (ipIsUnique) {
           bcryptIP(ipAddress).then((err, ipHash) => {
-            query[0].ipAddress.push(ipHash);
-            query[0].count += 1;
+            query[0].ipAddress.push(ipHash)
+            query[0].count += 1
             query[0].save()
-            resolve(true);
+            resolve(true)
           })
         }
       }
-    });
-  });
-  return promise;
+    })
+  })
+  return promise
 }
