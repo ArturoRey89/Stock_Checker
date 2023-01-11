c6d5354545d5b8d08266442dd8da2fa470206a93
ArturoRey89 <arturoreyes89@gmail.com>
2023-01-11 17:26:43 +0100

    fix: removed test for res.body.likeRegistered

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,11 @@
-const chaiHttp = require("chai-http");
-const chai = require("chai");
-const assert = chai.assert;
-const server = require("../server");
+const chaiHttp = require("chai-http")
+const chai = require("chai")
+const assert = chai.assert
+const server = require("../server")
 const testStock1 = "GOOG"
 const testStock2 = "AMZN"
 const ipAddress = "192.168.2.1"
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
 suite("Functional Tests", function () {
   suite("Viewing one stock:", function () {
@@ -16,14 +16,13 @@ suite("Functional Tests", function () {
         .set("X-Forwarded-For", ipAddress)
         .query({ stock: testStock1 })
         .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.stockData.stock, testStock1);
-          assert.isNumber(res.body.stockData.price);
-          assert.isNumber(res.body.stockData.likes);
-          assert.isFalse(res.body.likeRegistered);
-          done();
-        });
-    });
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData.stock, testStock1)
+          assert.isNumber(res.body.stockData.price)
+          assert.isNumber(res.body.stockData.likes)
+          done()
+        })
+    })
     test("GET request to /api/stock-prices/ and liking it", function (done) {
       chai
         .request(server)
@@ -31,14 +30,13 @@ suite("Functional Tests", function () {
         .set("X-Forwarded-For", ipAddress)
         .query({ stock: testStock1, like: "true" })
         .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.stockData.stock, testStock1);
-          assert.isNumber(res.body.stockData.price);
-          assert.isNumber(res.body.stockData.likes);
-          assert.isTrue(res.body.likeRegistered);
-          done();
-        });
-    });
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData.stock, testStock1)
+          assert.isNumber(res.body.stockData.price)
+          assert.isNumber(res.body.stockData.likes)
+          done()
+        })
+    })
     test("GET request to /api/stock-prices/ and liking it again", function (done) {
       chai
         .request(server)
@@ -46,15 +44,14 @@ suite("Functional Tests", function () {
         .set("X-Forwarded-For", ipAddress)
         .query({ stock: testStock1, like: "true" })
         .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.stockData.stock, testStock1);
-          assert.isNumber(res.body.stockData.price);
-          assert.isNumber(res.body.stockData.likes);
-          assert.isFalse(res.body.likeRegistered);
-          done();
-        });
-    });
-  });
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData.stock, testStock1)
+          assert.isNumber(res.body.stockData.price)
+          assert.isNumber(res.body.stockData.likes)
+          done()
+        })
+    })
+  })
   suite("Viewing two stocks", function () {
     test("GET request to /api/stock-prices/", function (done) {
       chai
@@ -63,21 +60,20 @@ suite("Functional Tests", function () {
         .set("X-Forwarded-For", ipAddress)
         .query({ stock: [testStock1, testStock2] })
         .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.stockData[0].stock, testStock1);
-          assert.isNumber(res.body.stockData[0].price);
-          assert.isNumber(res.body.stockData[0].rel_likes);
-          assert.equal(res.body.stockData[1].stock, testStock2);
-          assert.isNumber(res.body.stockData[1].price);
-          assert.isNumber(res.body.stockData[1].rel_likes);
-          assert.isFalse(res.body.likeRegistered);
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData[0].stock, testStock1)
+          assert.isNumber(res.body.stockData[0].price)
+          assert.isNumber(res.body.stockData[0].rel_likes)
+          assert.equal(res.body.stockData[1].stock, testStock2)
+          assert.isNumber(res.body.stockData[1].price)
+          assert.isNumber(res.body.stockData[1].rel_likes)
           assert.strictEqual(
             res.body.stockData[0].rel_likes,
             -res.body.stockData[1].rel_likes
-          );
-          done();
-        });
-    });
+          )
+          done()
+        })
+    })
     test("GET request to /api/stock-prices/ and liking them", function (done) {
       chai
         .request(server)
@@ -85,23 +81,22 @@ suite("Functional Tests", function () {
         .set("X-Forwarded-For", ipAddress)
         .query({ stock: [testStock1, testStock2], like: "true" })
         .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.stockData[0].stock, testStock1);
-          assert.isNumber(res.body.stockData[0].price);
-          assert.isNumber(res.body.stockData[0].rel_likes);
-          assert.equal(res.body.stockData[1].stock, testStock2);
-          assert.isNumber(res.body.stockData[1].price);
-          assert.isNumber(res.body.stockData[1].rel_likes);
-          assert.isTrue(res.body.likeRegistered);
+          assert.equal(res.status, 200)
+          assert.equal(res.body.stockData[0].stock, testStock1)
+          assert.isNumber(res.body.stockData[0].price)
+          assert.isNumber(res.body.stockData[0].rel_likes)
+          assert.equal(res.body.stockData[1].stock, testStock2)
+          assert.isNumber(res.body.stockData[1].price)
+          assert.isNumber(res.body.stockData[1].rel_likes)
           assert.strictEqual(
             res.body.stockData[0].rel_likes,
             -res.body.stockData[1].rel_likes
-          );
-          done();
-        });
-    });
-  });
-});
+          )
+          done()
+        })
+    })
+  })
+})
 
 // GET single reult: { stockData: { stock: "GOOG", price: 786.9, likes: 1 } };
 // GET compare two result: { stockData: [ { stock: "MSFT", price: 62.3, rel_likes: -1 }, { stock: "GOOG", price: 786.9, rel_likes: 1 } ] };
