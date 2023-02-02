4f52df186b7e641e2aeee70459b03a2c25f738b5
ArturoRey89 <arturoreyes89@gmail.com>
2023-02-02 12:46:17 +0100

    fix: async function in addLikes

--- routes/api.js
+++ routes/api.js
@@ -40,52 +40,59 @@ module.exports = function (app) {
 
     //Compare Stocks
     if (Array.isArray(stock)) {
-      console.log("Compare stocks:")
       if (like === "true") {
-        addLikes(stock[0], req.headers["x-forwarded-for"]).then((likeAdded) =>
-          console.log("like added: ", likeAdded)
-        )
-        addLikes(stock[1], req.headers["x-forwarded-for"]).then((likeAdded) =>
-          console.log("like added: ", likeAdded)
-        )
+        let ip = req.headers["x-forwarded-for"]
+        addLikes(stock[0], ip)
+          .then((likeAdded) => console.log("like added: ", likeAdded))
+          .catch((error) => console.log("addLikes error: ", error))
+        addLikes(stock[1], ip)
+          .then((likeAdded) => console.log("like added: ", likeAdded))
+          .catch((error) => console.log("addLikes error: ", error))
       }
       Promise.all([
         getStockPrice(stock[0]),
         getLikes(stock[0]),
         getStockPrice(stock[1]),
         getLikes(stock[1]),
-      ]).then((results) => {
-        let [stock1, likes1, stock2, likes2] = results
-        res.json({
-          stockData: [
-            {
-              stock: stock1.symbol,
-              price: stock1.price,
-              rel_likes: likes1 - likes2,
-            },
-            {
-              stock: stock2.symbol,
-              price: stock2.price,
-              rel_likes: likes2 - likes1,
-            },
-          ],
+      ])
+        .then((results) => {
+          let [stock1, likes1, stock2, likes2] = results
+          res.json({
+            stockData: [
+              {
+                stock: stock1.symbol,
+                price: stock1.price,
+                rel_likes: likes1 - likes2,
+              },
+              {
+                stock: stock2.symbol,
+                price: stock2.price,
+                rel_likes: likes2 - likes1,
+              },
+            ],
+          })
         })
-      })
+        .catch((error) => console.log("Promise.all error: ", error))
     }
     //Get single stock
     if (!Array.isArray(stock)) {
       if (like === "true") {
-        addLikes(stock, req.headers["x-forwarded-for"]).then((likeAdded) =>
-          console.log("like added: ", likeAdded)
-        )
+        addLikes(stock, req.headers["x-forwarded-for"])
+          .then((likeAdded) => console.log("like added: ", likeAdded))
+          .catch((error) => console.log("addLikes error: ", error))
       }
-      console.log("View stock:")
-      Promise.all([getStockPrice(stock), getLikes(stock)]).then((results) => {
-        let [stock, likes] = results
-        res.json({
-          stockData: { stock: stock.symbol, price: stock.price, likes: likes },
+      Promise.all([getStockPrice(stock), getLikes(stock)])
+        .then((results) => {
+          let [stock, likes] = results
+          res.json({
+            stockData: {
+              stock: stock.symbol,
+              price: stock.price,
+              likes: likes,
+            },
+          })
         })
-      })
+        .catch((error) => console.log("Promise.all error: ", error))
     }
   })
 }
@@ -113,6 +120,7 @@ const bcryptIP = (ipAddress) => {
   const promise = new Promise((resolve, reject) => {
     bcrypt.hash(ipAddress, process.env.SALT * 1, (err, hash) => {
       if (err) {
+        console.log("bcrypt.hash() error: ", err)
         reject(err)
       }
       if (hash) {
@@ -161,7 +169,7 @@ const addLikes = (stockName, ipAddress) => {
               if (err) {
                 reject(err)
               } else {
-                resolve(true)
+                resolve([true, "New stock added: ", stockLikes])
               }
             }
           )
@@ -170,33 +178,41 @@ const addLikes = (stockName, ipAddress) => {
       // check if IP is new, and add like if new
       else {
         let ipIsUnique = true
+        let compareIpAddressArray = []
         query[0].ipAddress.forEach((ipHash) => {
-          bcrypt.compare(ipAddress, ipHash, (err, match) => {
-            if (err) {
-              reject(err)
-            }
-            if (match) {
-              ipIsUnique = false
-            }
-          })
+          compareIpAddressArray.push(
+            bcrypt.compare(ipAddress, ipHash, (err, match) => {
+              if (err) {
+                console.log("bcrypt.compare Error: ", err)
+                reject(err)
+              }
+              if (match) {
+                ipIsUnique = false
+                resolve([false, "IP already registered"])
+              }
+            })
+          )
         })
-
-        if (!ipIsUnique) {
-          resolve(false)
-        }
-
-        if (ipIsUnique) {
-          bcryptIP(ipAddress).then((err, ipHash) => {
-            if (err) {
-              reject(err)
-            } else {
-              query[0].ipAddress.push(ipHash)
-              query[0].count += 1
-              query[0].save()
-              resolve(true)
+        Promise.all(compareIpAddressArray)
+          .then(() => {
+            if (ipIsUnique) {
+              bcryptIP(ipAddress)
+                .then((ipHash) => {
+                  query[0].ipAddress.push(ipHash)
+                  query[0].count += 1
+                  query[0].save()
+                  resolve([true, "New like added, ip:", ipAddress])
+                })
+                .catch((err) => {
+                  console.log("bcryptIP Error: ", err)
+                  reject(err)
+                })
             }
           })
-        }
+          .catch((err) => {
+            console.log("Promise.all error", err)
+            reject(err)
+          })
       }
     })
   })
